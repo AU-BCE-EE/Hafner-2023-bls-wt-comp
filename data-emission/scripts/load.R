cfdbe1a8cf565ac09e6aa2b5ce3112543d79eb4a
Sasha D. Hafner <sasha@hafnerconsulting.com>
2022-12-06 14:11:01 -0500

    Get latest ALFAM2-data data w JNK gap-filling note

--- data-emission/scripts/load.R
+++ data-emission/scripts/load.R
@@ -1,4 +1,4 @@
 
-p <- paste0('https://github.com/sashahafner/ALFAM2-data/raw/', rtag)
+p <- paste0(ghpath, rtag)
 idat <- fread(paste0(p, '/data-output/03/ALFAM2_interval.csv.gz'))
 pdat <- fread(paste0(p, '/data-output/03/ALFAM2_plot.csv.gz'))
--- data-emission/scripts/main.R
+++ data-emission/scripts/main.R
@@ -3,7 +3,10 @@
 rm(list = ls())
 
 # Set release tag for download
-rtag <- 'v2.11'
+#ghpath <- 'https://github.com/sashahafner/ALFAM2-data/raw/'
+ghpath <- 'https://github.com/sashahafner/ALFAM2-data/raw/bf608a10d324444627acf35f0aadeaced5f008c9/'
+#rtag <- 'v2.11'
+rtag <- ''
 
 source('packages.R')
 source('load.R')
