9613b1f045a28ac9e810b8d8a4e8ea37ae23cf5e
Sasha D. Hafner <sasha@hafnerconsulting.com>
2022-12-19 19:07:41 -0500

    Download release v2.16 of ALFAM2 data

--- data-emission/scripts/main.R
+++ data-emission/scripts/main.R
@@ -3,10 +3,12 @@
 rm(list = ls())
 
 # Set release tag for download
-#ghpath <- 'https://github.com/sashahafner/ALFAM2-data/raw/'
-ghpath <- 'https://github.com/sashahafner/ALFAM2-data/raw/bf608a10d324444627acf35f0aadeaced5f008c9/'
-#rtag <- 'v2.11'
-rtag <- ''
+ghpath <- 'https://github.com/sashahafner/ALFAM2-data/raw/'
+rtag <- 'v2.16'
+
+# Alternative below to get particular commit (if not yet in release)
+#ghpath <- 'https://github.com/sashahafner/ALFAM2-data/raw/bf608a10d324444627acf35f0aadeaced5f008c9/'
+#rtag <- ''
 
 source('packages.R')
 source('load.R')
