2412ae888ee98240cae53a169a03462d89703d9f
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-05-31 11:32:42 -0400

    Add database version

--- data-emission/scripts/load.R
+++ data-emission/scripts/load.R
@@ -2,3 +2,6 @@
 p <- paste0(ghpath, rtag)
 idat <- fread(paste0(p, '/data-output/03/ALFAM2_interval.csv.gz'))
 pdat <- fread(paste0(p, '/data-output/03/ALFAM2_plot.csv.gz'))
+
+# Get file with data version number
+download.file(paste0(p, '/data-output/03/data_version.txt'), destfile = '../logs/data_version.txt')
