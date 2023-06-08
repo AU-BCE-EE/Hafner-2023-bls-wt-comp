58d2d1e57c7748bf5a659bebd9bc989be94e960a
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2023-06-08 20:19:08 +0200

    added scripts-WT

---
+++ scripts-WT/main.R
@@ -0,0 +1,8 @@
+
+####################################################
+
+source('packages.R')
+
+source('21C.R') # first august trial
+source('21D.R') # second august trial
+source('22A.R') # january trial
