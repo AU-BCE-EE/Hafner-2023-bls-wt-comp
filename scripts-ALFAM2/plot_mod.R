9806f49bd8837d8ab1a4a4241032aaf2eb76d6ed
Sasha D. Hafner <sasha.hafner@gmail.com>
2023-09-10 07:52:29 +0200

    New plot for RAMIRAN

--- scripts-ALFAM2/plot_mod.R
+++ scripts-ALFAM2/plot_mod.R
@@ -82,6 +82,20 @@ ggplot(dw, aes(bta, value, group = pmid)) +
   theme(legend.position = 'top')
 ggsave2x('../plots-ALFAM2/54_flux_comp_sel_zoom', height = 5, width = 7)
 
+ddw <- dw[dw$variable != 'j.pred2', ]
+ddb <- db[db$variable != 'j.pred2', ]
+ggplot(ddw, aes(bta, value, group = pmid)) +
+  geom_step(aes(colour = aer), lwd = 0.5, alpha = 0.8) +
+  geom_step(data = ddb, aes(lty = meas.tech), lwd = 0.5, alpha = 0.8, colour = 'red') +
+  facet_grid(variable.nm ~ app.date) +
+  coord_cartesian(xlim = c(0, 50), ylim =c(0, 5.5)) +
+  #scale_colour_viridis_c(option = 'H') +
+  theme_bw() +
+  labs(x = 'Elapsed time (h)', y = expression('NH'[3]~'flux'~('kg N h'^'-1'~ha^'-1')), 
+       colour = expression(atop('Wind tunnel AER'~(min^'-1'),' ')), lty = ' ') +
+  theme(legend.position = 'top')
+ggsave2x('../plots-ALFAM2/57_flux_comp_sel_zoom', height = 3.3, width = 7)
+
 # Drop sets 2 and A for paper
 dd <- subset(dfl, bta >= -2 & !variable %in% c('j.pred2', 'j.preda'))
 dd$j.NH3[grepl('i', dd$flag.int)] <- NA
